a7c984ca5b2dde5b75b03057e1e1cf5ba51f166a
sparku7 <sparku7@sky.com>
2024-05-29 14:21:01 +0100

    Updated toggle

--- todo_list/src/component/ThemedComponent.jsx
+++ todo_list/src/component/ThemedComponent.jsx
@@ -1,10 +1,8 @@
-//using this hook, we now gain access to consume the theme context (everything within the file)
-//we passed theme and toggle theme functions to the ThemeContext.Provider
 import { useTheme } from '../pages/ThemeContext'
 
 const ThemedComponent = () => {
 
-    //where does the toggle come from
+   
     const {theme, toggleTheme} = useTheme();
 
     return(
