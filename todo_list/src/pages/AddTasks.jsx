8cd05d1ddfe1a588dd99054df4468010379c11c3
sparku7 <sparku7@sky.com>
2024-05-29 16:20:25 +0100

    added alert

--- todo_list/src/pages/AddTasks.jsx
+++ todo_list/src/pages/AddTasks.jsx
@@ -21,6 +21,8 @@ const Create = () => {
                 body: JSON.stringify(user)
             })
 
+            alert("Your task has been added");
+
         setTask('')
         setDescription('')
         setImageUrl('')
