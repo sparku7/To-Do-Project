59250f67e93bc6f84bf80f4d61226fbedd4e2286
sparku7 <sparku7@sky.com>
2024-05-29 16:17:04 +0100

    some css tweaks

--- todo_list/src/pages/HomePage.jsx
+++ todo_list/src/pages/HomePage.jsx
@@ -5,7 +5,8 @@ import '../CSSfiles/HomePage.css'
 export default function HomePage() {
     return (
         <div>
-            <h3>Fill this in later</h3>
+            <h3>To-do lists offer a way to increase productivity, stopping you from forgetting things, helps prioritise tasks, manage tasks effectively, use time wisely and improve time management as well as workflow.</h3>
+            <img src='https://th.bing.com/th/id/R.6817054f7fc9ce047ef43f8c0d373fac?rik=zhYGpaGQwVKqpg&riu=http%3a%2f%2fstraightenyourpaths.com%2fwp-content%2fuploads%2f2013%2f01%2fbigstock-Blank-To-Do-List-27249434.jpg&ehk=FlDQGQ%2fIIFYypdRHSqtNPst5VVAbdtTNiaZVNLOnVho%3d&risl=&pid=ImgRaw&r=0' />
         </div>
     )
 }
